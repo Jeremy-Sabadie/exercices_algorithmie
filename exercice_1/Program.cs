124a3f79dc30bc3d4a705f432f7f01e54455bca4
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2022-11-29 14:46:49 +0100

    Ajoutez des fichiers projet.

---
+++ exercice_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bonjour toi!");
+            string Name= Console.ReadLine();
+            Console.WriteLine("bonjour "+Name+".");
+
+            Console.ReadLine();
+        }
+    }
+}

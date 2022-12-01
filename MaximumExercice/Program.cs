e0cf3fb226e6836d61be5d5a35f72fe3f81c9dca
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2022-12-01 11:15:48 +0100

    write , not writeline

---
+++ MaximumExercice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumExercice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indiquez un premier nombre à comparer");//Un premier entier est demandé en entrée
+            int Nb_1 = int.Parse(Console.ReadLine());//La première entrée est attendue.
+            Console.Write("Indiquez un deuxième nombre à comparer");//Un deuxième entier est demandé en entrée
+            int Nb_2 = int.Parse(Console.ReadLine());//La deuxième entrée est attendue.
+            if (Nb_1 > Nb_2)//
+            {
+                Console.WriteLine(Nb_1 + "est plus grand que" + Nb_2);//Le résultat de la comparison est affichée avec la première possibilité.
+            }
+            else//L'autre possibilité de résultat de la comparaison.
+            {
+                Console.WriteLine(Nb_2 + " est supérieur à " + Nb_1);//Le résultat de la comparison est affichée avec la deuxième possibilité.
+            }
+            
+        }
+    }
+}
--- pair_xercice/Program.cs
+++ pair_xercice/Program.cs
@@ -10,16 +10,16 @@ namespace pair_xercice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Donnez un nombre à vérifier. ");
+            Console.Write("Donnez un nombre à vérifier: ");
             string Ask = Console.ReadLine();//Un nombre est demandé en entrée. 
             int Nb = int.Parse(Ask);//L'entrée de l'utilisateur est convertie en entier.
             if (Nb % 2 == 0) //On regarde si le modulo du nombre edivisé par deux est égal à zéro, alors le nombre est 
             {
-                Console.WriteLine(Nb+" est un nombre pair"); //On indique que le nombre doné est pair.
+                Console.Writeline(Nb+" est un nombre pair."); //On indique que le nombre doné est pair.
             }
             else //sinon si le modu lo de la division par deux n'est pas égal à zéro. 
             {
-                Console.WriteLine(Nb+" est un nombre impair"); //On indique que le nombre doné est impair.
+                Console.WriteLine(Nb+" est un nombre impair."); //On indique que le nombre doné est impair.
             }
             Console.ReadLine();
 

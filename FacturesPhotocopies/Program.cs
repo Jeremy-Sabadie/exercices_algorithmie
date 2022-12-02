345f984a9a0179087e52ae1acf13216afe48f386
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2022-12-02 09:03:26 +0100

    readline at the end.

---
+++ FacturesPhotocopies/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturesPhotocopies
+{
+    internal class Program
+    //Un magasin de reprographie facture 0,10 E les dix premières photocopies, 0,09 E les vingt suivantes et 0,08 E au-delà.
+    //Écrivez un algorithme qui demande à l’utilisateur le nombre de photocopies effectuées et qui affiche la facture correspondante.
+    {
+        static void Main(string[] args)
+        {
+            int NbCopy = int.Parse(Console.ReadLine());// 
+            Console.Write("Inndiquez le nombre de photocopies");//
+            const double FirstPrice = 0.10;//Un prix est demandé pour les dix premières copies
+            const double SecondPrice = 0.8;//Un second prix est stocké pour les vingt copies suivantes. 
+            double Facture = (NbCopy - 10) *
+                SecondPrice + (10 * FirstPrice);
+            Console.WriteLine("le montant de la facture est de : " +);
+
+        }
+    }
+}
--- MaximumExercice/Program.cs
+++ MaximumExercice/Program.cs
@@ -16,12 +16,13 @@ namespace MaximumExercice
             int Nb_2 = int.Parse(Console.ReadLine());//La deuxième entrée est attendue.
             if (Nb_1 > Nb_2)//
             {
-                Console.WriteLine(Nb_1 + "est plus grand que" + Nb_2);//Le résultat de la comparison est affichée avec la première possibilité.
+                Console.WriteLine(Nb_1 + "est plus grand que " + Nb_2);//Le résultat de la comparison est affichée avec la première possibilité.
             }
             else//L'autre possibilité de résultat de la comparaison.
             {
                 Console.WriteLine(Nb_2 + " est supérieur à " + Nb_1);//Le résultat de la comparison est affichée avec la deuxième possibilité.
             }
+            Console.ReadLine();//ligne pour ne pas que la console se ferme à la fin du programe.
             
         }
     }

65863393ee16bfc1ac5c5b861ead527c9b0a762f
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2022-12-02 11:27:49 +0100

    comment and optimisation

---
+++ 10_signe_dun_produit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_signe_dun_produit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("indiquez un premier nombre différent de zéro :");//Un premier nombre est demandé.
+           int Nb1= int.Parse(Console.ReadLine());//Un premier nombre est attendu.
+            Console.Write("indiquez un deuxième nombre différent de zéro :");//Un deuxième nombre est demandé.
+            int Nb2 = int.Parse(Console.ReadLine());//Un deuxième nombre est attendu.
+
+            if ((Nb1 > 0 && Nb2 > 0)|| (Nb1 < 0 & Nb2 < 0))//Si (Nb1supérieur à zéro et Nb1 supérieur à zéro et Nb2 supérieur à zéro) ou (Nb1 inférieur à zéro et Nb2 inférieur à zéro)
+            {
+                Console.WriteLine("la somme des deux nombre est positive.");//On indique que la somme des deux variable est positive.
+            
+            }
+
+            
+                
+            
+            else//sinon s i la con dition n'est pas remplie.
+            {
+                Console.WriteLine("la somme des deux nombre est négative.");//On indique que la somme des deux variable est négative.
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}

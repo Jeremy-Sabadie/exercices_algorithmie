30f17d405eb3faaf05a4239bbef165878b4a9e69
cda07saba <jeremy.sabadie@2isa.org>
2022-12-07 09:28:42 +0100

    just one condition in the impôt zorglub exercice.

--- 13.impôt_zorglub/Program.cs
+++ 13.impôt_zorglub/Program.cs
@@ -11,17 +11,17 @@ int AgeConvert = int.Parse(Age);
 
 
 //Si sexe==femme et age supérieur ou égal à 18 et age inférieur ou égal à 35
-if (Genre == 'f' && AgeConvert >= 18 && AgeConvert <= 35)
+if ((Genre == 'f' && AgeConvert >= 18 && AgeConvert <= 35) || (Genre == 'h' && AgeConvert > 20))
 {
     Console.WriteLine("impôt!");
 }
 //On affiche  "impôt!"
 //Si sexe est éga à homme et agesupérieur à20:
-else if (Genre == 'h' && AgeConvert > 20)
-{
-    Console.WriteLine("impôt!");
-}
 else
+{
     Console.WriteLine("pas d'impôt");
+}
+
+
     //On affiche  "impôt!"
     Console.ReadLine();

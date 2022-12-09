46b02a4eafd9bf3f7bbb4a247d5babd33c1a5486
cda07saba <jeremy.sabadie@2isa.org>
2022-12-09 11:40:03 +0100

    somme de entiers de n.

---
+++ 18.Table de multiplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Table_de_multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("donnez un nombre:");
+            int Nb = int.Parse(Console.ReadLine());
+            int Mult = 1;
+            int Result = 1;
+            while (Mult <= 10)
+            {
+                Result = Nb * Mult;
+                Console.WriteLine($"     {Nb} X {Mult} est égal a: {Result}");
+                Mult++;
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

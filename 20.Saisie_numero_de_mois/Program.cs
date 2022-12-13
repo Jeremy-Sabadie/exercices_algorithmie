d35bf0a2ce552c3887469395813ba88c38337046
cda07saba <jeremy.sabadie@2isa.org>
2022-12-13 09:45:51 +0100

    2. mois_demandé do while et  while.

---
+++ 20.Saisie_numero_de_mois/Program.cs
@@ -0,0 +1,15 @@
+﻿//boucle while:
+//Console.Write("Indiquez un numéro de mois.");//On demande une entrée.
+//int Month = int.Parse(Console.ReadLine());//On récupère l'entrée dans la variable Month.
+                                          //while (!(Month >= 1 && Month <= 12))//Condition à vérifier(tant que Month n'est pas compris entre 1 et 12).
+                                          //{
+                                          //    Console.Write("Indiquez un numéro de mois.");//On re demande une entrée à l'utilisateur.
+//Month = int.Parse(Console.ReadLine());//Récupération de la nouvelle entrée dans la variable Month qui prend une nouvelle valeur.
+//}
+//Avec do while:
+int Month;
+do
+{
+    Console.Write("Indiquez un numéro de mois.");//Action à effectuer.
+    Month = int.Parse(Console.ReadLine());
+} while (!(Month >= 1 && Month <= 12));//Instruction à vérifier.

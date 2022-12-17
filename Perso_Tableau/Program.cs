ab2bc4a1ad626ad63c67f601b353e4c7d3d2f416
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2022-12-17 15:55:54 +0100

    Exercice des notes refait.

--- Perso_Tableau/Program.cs
+++ Perso_Tableau/Program.cs
@@ -1,4 +1,4 @@
-﻿int NBnotes = 5//Le nombre de notes est défini dan une variable
+﻿int NBnotes = 5;//Le nombre de notes est défini dan une variable
 int Sum = 0;//Cett variable servia à calculer la somme des notes du tableau pour calculer la moyenne des notes.
 int[] Array = new int[NBnotes];
 
@@ -8,5 +8,5 @@ for (int i = 0; i <NBnotes; i++)//Boucle for autant que la valeur de la varia bl
     Array[i] = int.Parse(Console.ReadLine());//Lentrée de l'utilisateur est stockée dans le tableau et un numéro d'index lui est attrbué.
     Sum = Sum + Array[i];//La nouvelle note est additionnée à la somme du tableau
 }
-int Moy = Sum / NBnotes//La somme des notes est divisée par le nombre de notes.
+int Moy = Sum / NBnotes;//La somme des notes est divisée par le nombre de notes.
 Console.WriteLine($"lamoyenne des notes est de:{Moy}.");//le résultat est affiché en console.

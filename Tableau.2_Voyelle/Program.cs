e40d7d914aa3d98c5e4f30f284ef060a3b1c3a04
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2022-12-18 14:15:15 +0100

    Tableau.2 _Voyelle exercice done.

---
+++ Tableau.2_Voyelle/Program.cs
@@ -0,0 +1,8 @@
+﻿char[] Array = { 'A', 'E', 'I', 'O', 'U', 'Y' };//le tableau avec six charactères est déclaré.
+for (int i = 0; i <Array.Length; i++)//Le tableau est parcourru autant qu'il contient d'items.
+{
+    Console.Write(Array[i]);//L'item récupéré au tour de boucle est affiché en console.
+    
+}
+
+

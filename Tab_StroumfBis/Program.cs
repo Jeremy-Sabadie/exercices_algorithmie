d4af15f8d088e8e1c1929f1679726ccf3acb1de3
cda07saba <jeremy.sabadie@2isa.org>
2023-01-03 14:38:43 +0100

    Stroumpf_bis to practice.

---
+++ Tab_StroumfBis/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] Array1 = { 4, 8, 7, 12 };
+int[] Array2 = { 3, 6 };
+int Sum_T1 = SumArray(Array1);
+int Sum_T2 = SumArray(Array2);
+
+int Stroumf = Sum_T1 * Sum_T2;
+int SumArray(int[] Tab)
+{
+    {
+        int Total = 0;
+        for (int i = 0; i < Tab.Length; i++)
+        {
+            Total = Total + Tab[i];
+        }
+        return Total;
+    }
+    
+
+
+}
+Console.WriteLine($"La valeur du Stroumpf est de: {Stroumf}.");

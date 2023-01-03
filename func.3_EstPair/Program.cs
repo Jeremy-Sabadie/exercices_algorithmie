abce812fbde2c7d42a0a663825dc245749425f7f
cda07saba <jeremy.sabadie@2isa.org>
2023-01-03 11:46:46 +0100

    func.3_EstPair done.

--- func.3_EstPair/Program.cs
+++ func.3_EstPair/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int IsPair(int Nb)
+{
+    bool Pair = false;
+
+    if(Nb % 2 == 0) { Pair= true; }
+    if(Pair==true){ Console.WriteLine($"Le nombre: {Nb} est pair."); }
+    else if(Pair==false) { Console.WriteLine($"Le nombre: {Nb} est impair."); }
+    return Nb;
+}
+IsPair(43);
+IsPair(42);
+

0d157660422d36300a6b1a1d6fb7343058b32f4f
cda07saba <jeremy.sabadie@2isa.org>
2023-01-03 11:57:10 +0100

    Comment adding on func.3 exercice.

--- func.3_EstPair/Program.cs
+++ func.3_EstPair/Program.cs
@@ -1,12 +1,18 @@
-﻿int IsPair(int Nb)
+﻿//déclaration de la fonction.
+int IsPair(int Nb)
 {
     bool Pair = false;
-
+//Analyse du nombre à l'aide du modulo.
     if(Nb % 2 == 0) { Pair= true; }
+    //Afichge du résultat.
     if(Pair==true){ Console.WriteLine($"Le nombre: {Nb} est pair."); }
     else if(Pair==false) { Console.WriteLine($"Le nombre: {Nb} est impair."); }
     return Nb;
-}
+}//Apel de la fonction avec une entrée utilisateur en paramètre.
+Console.Write("indiquez un nombre.");
+int Number= int.Parse(Console.ReadLine());
+//Apel de la fonction avec des nombres donnés.
 IsPair(43);
 IsPair(42);
+IsPair(Number);
 

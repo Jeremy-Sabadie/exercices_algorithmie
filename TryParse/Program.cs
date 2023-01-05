8ad9a14956833aa85fcad9126daba5801814628e
cda07saba <jeremy.sabadie@2isa.org>
2023-01-05 11:40:42 +0100

    TryParse.

---
+++ TryParse/Program.cs
@@ -0,0 +1,10 @@
+﻿
+bool ParseOK;
+int Nombre;
+do
+{
+    Console.WriteLine("Entrez un nombre: ");
+    ParseOK = int.TryParse(Console.ReadLine
+        (), out Nombre);
+} while (ParseOK == false);
+    Console.WriteLine(Nombre);
